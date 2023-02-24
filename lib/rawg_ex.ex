2a17f733672061501e5670e53e6aac1b68af4d42
Javier Garea Cidre <javier.garea@udc.es>
2023-02-24 20:15:48 +0100

    feat(#1): get creator roles
    closes #1

--- lib/rawg_ex.ex
+++ lib/rawg_ex.ex
@@ -1,16 +1,24 @@
 defmodule RawgEx do
+  alias RawgEx.Error
+
   @moduledoc """
   Documentation for `RawgEx`.
   """
 
   # -------------------------------------------------------------------------------
+  # Constants
+  # -------------------------------------------------------------------------------
+  @url_prefix "https://api.rawg.io/api/"
+
+  # -------------------------------------------------------------------------------
   # Types
   # -------------------------------------------------------------------------------
-  @type opt(t) :: nil | t
+  @type name() :: atom()
 
   @type position() :: %{id: integer(), name: String.t(), slug: String.t()}
 
-  @type start_link_opts() :: [{:name, atom()}]
+  @type opt(t) :: nil | t
+  @type start_link_opts() :: [{:name, name()}, {:pools, map()}]
   @type get_creator_roles_opts() :: [{:page, non_neg_integer()}, {:page_size, non_neg_integer()}]
 
   # -------------------------------------------------------------------------------
@@ -18,15 +26,37 @@ defmodule RawgEx do
   # -------------------------------------------------------------------------------
   @spec start_link(opts :: start_link_opts()) :: Supervisor.on_start()
   def start_link(opts) do
-    name = Keyword.get(opts, :name) || raise(ArgumentError, "must supply a name")
-    Finch.start_link(name: name)
+    Finch.start_link(opts)
   end
 
-  @spec get_creator_roles(opts :: get_creator_roles_opts()) :: %{
-          count: integer(),
-          next: opt(String.t()),
-          previous: opt(String.t()),
-          results: [position()]
-        }
-  def get_creator_roles(_opts), do: raise("not implemented")
+  @spec get_creator_roles(name :: name(), opts :: get_creator_roles_opts()) ::
+          {:ok,
+           %{
+             count: non_neg_integer(),
+             next: opt(String.t()),
+             previous: opt(String.t()),
+             results: [position()]
+           }}
+          | {:error, RawgEx.Error.t()}
+  def get_creator_roles(name, opts \\ []) do
+    api_key = Application.get_env(__MODULE__, :api_key)
+    query_string = URI.encode_query([{:key, api_key} | opts])
+    url = "#{@url_prefix}creator-roles?#{query_string}"
+    response = Finch.build(:get, url) |> Finch.request(name)
+
+    case response do
+      {:ok,
+       %Finch.Response{
+         body: body,
+         status: 200
+       }} ->
+        {:ok, Jason.decode!(body, keys: :atoms)}
+
+      {:ok,
+       %Finch.Response{
+         status: status
+       }} ->
+        {:error, Error.decode(status)}
+    end
+  end
 end
--- test/rawg_ex_test.exs
+++ test/rawg_ex_test.exs
@@ -1,9 +1,51 @@
 defmodule RawgExTest do
   use ExUnit.Case
+  import Mock
   doctest RawgEx
 
-  test "starts" do
-    {:ok, pid} = RawgEx.start_link(name: :test)
-    assert pid |> is_pid()
+  @test_name :rawg_ex_test
+
+  setup_all do
+    RawgEx.start_link(name: @test_name)
+    :ok
+  end
+
+  describe "creator roles" do
+    test "correctly gets creator roles" do
+      creator_roles = %{
+        count: 1,
+        next: nil,
+        previous: nil,
+        results: [%{id: 1, name: "test", slug: "test"}]
+      }
+
+      with_mock Finch, [:passthrough],
+        request: fn _request, _name ->
+          body = Jason.encode!(creator_roles)
+
+          {:ok,
+           %Finch.Response{
+             body: body,
+             headers: [],
+             status: 200
+           }}
+        end do
+        assert {:ok, creator_roles} == RawgEx.get_creator_roles(@test_name)
+      end
+    end
+
+    test "properly produces errors" do
+      with_mock Finch, [:passthrough],
+        request: fn _request, _name ->
+          {:ok,
+           %Finch.Response{
+             body: nil,
+             headers: [],
+             status: 401
+           }}
+        end do
+        assert {:error, :unauthorized} == RawgEx.get_creator_roles(@test_name)
+      end
+    end
   end
 end
